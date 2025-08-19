b75940a2247c32580e422ee41d6a83e1d8933e40
dancingcodes <dancingcodes@qq.com>
2025-08-19 17:54:45 +0800

    1

--- pages/video/video.vue
+++ pages/video/video.vue
@@ -1,18 +1,26 @@
 <template>
-	<div class="video" @touchstart="onTouchStart" @touchend="onTouchEnd">
-		<div class="videoScrollBox" v-for="(i, index) in videoList" :key="i.id" v-show="currentIndex === index">
-			<video class="videoItem" :src="i.videoSrc" :id="`video${i.id}`" loop :controls="false"></video>
+	<div class="video" @touchstart="onTouchStart" @touchmove="onTouchMove" @touchend="onTouchEnd">
+		<div class="scrollBox"
+			:style="{top: `${scrollData.currentPage * -100}%`, transform: `translateY(${scrollData.moveY * -1}px)`}">
+			<video class="videoItem" v-for="i in videoList" :src="i.videoSrc" :id="`video${i.id}`" loop
+				:controls="false"></video>
 		</div>
 	</div>
 </template>
 
 <script>
+	import {
+		video
+	} from '@/api/index.js'
 	export default {
 		data() {
 			return {
 				videoList: [],
-				currentIndex: 0,
-				startY: 0
+				scrollData: {
+					currentPage: 1,
+					startY: 0,
+					moveY: 0
+				}
 			}
 		},
 		onLoad() {
@@ -21,9 +29,9 @@
 		methods: {
 			getList() {
 				const res = [
-					"https://www.w3school.com.cn/i/movie.mp4",
-					"https://media.w3.org/2010/05/sintel/trailer.mp4",
-					"https://media.w3.org/2010/05/bunny/trailer.mp4"
+					"https://alimov2.a.kwimgs.com/upic/2023/12/21/16/BMjAyMzEyMjExNjEzNDNfMTAyNzY2NDk1MV8xMjAxNzc0MzgyNTdfMV8z_b_B1fa8b1158ae14559830d8ade4f510f02.mp4?clientCacheKey=3x6jqjbe9pye39s_b.mp4&tt=b&di=78e498d6&bp=14214",
+					"https://alimov2.a.kwimgs.com/upic/2023/11/24/11/BMjAyMzExMjQxMTQ1NThfODEwNTYyNjg3XzExNzkxNTA1Mzc3M18xXzM=_b_B8627dfbe86e0f53c3b853aa47b496549.mp4?clientCacheKey=3xn7devj75c23u6_b.mp4&tt=b&di=78e49864&bp=13414",
+					"https://alimov2.a.kwimgs.com/upic/2022/10/16/10/BMjAyMjEwMTYxMDAxMTJfODczNTUwN184NjU1MzgxODQ2OF8xXzM=_b_B622d93ca0b9275a8e533ea16686b9399.mp4?clientCacheKey=3xe59hvcyy2jumi_b.mp4&tt=b&di=78e49e79&bp=13414",
 				]
 
 				this.videoList = res.map(src => ({
@@ -36,49 +44,33 @@
 					this.videoList.forEach(i => {
 						i.context = uni.createVideoContext('video' + i.id, this)
 					})
-					// 播放第一个视频
-					this.playVideo(0)
 				})
 			},
 			onTouchStart(e) {
-				this.startY = e.touches[0].clientY
+				this.scrollData.startY = e.touches[0].clientY
 			},
-			onTouchEnd(e) {
-				const endY = e.changedTouches[0].clientY
-				const diff = endY - this.startY
+			onTouchMove(e) {
+				this.scrollData.moveY = this.scrollData.startY - e.changedTouches[0].clientY
+			},
 
-				if (diff > 50) {
-					// 下滑
-					this.prevVideo()
-				} else if (diff < -50) {
-					// 上滑
-					this.nextVideo()
+			onTouchEnd() {
+				const y = this.scrollData.moveY
+
+				this.scrollData.moveY = 0
+				if (y >= 100) {
+					if(this.scrollData.currentPage >= 2) {
+						return
+					}
+					this.scrollData.currentPage++
 				}
-			},
-			nextVideo() {
-				const next = this.currentIndex + 1
-				if (next < this.videoList.length) {
-					this.switchVideo(next)
+
+				if (y <= -100) {
+					if(this.scrollData.currentPage <= 0) {
+						return
+					}
+					this.scrollData.currentPage--
 				}
-			},
-			prevVideo() {
-				const prev = this.currentIndex - 1
-				if (prev >= 0) {
-					this.switchVideo(prev)
-				}
-			},
-			switchVideo(index) {
-				this.pauseVideo(this.currentIndex)
-				this.currentIndex = index
-				this.playVideo(this.currentIndex)
-			},
-			playVideo(index) {
-				const i = this.videoList[index]
-				if (i?.context) i.context.play()
-			},
-			pauseVideo(index) {
-				const i = this.videoList[index]
-				if (i?.context) i.context.pause()
+
 			}
 		}
 	}
@@ -93,16 +85,16 @@
 		height: 100%;
 		overflow: hidden;
 
-		.videoScrollBox {
-			width: 100%;
-			height: 100%;
-			position: absolute;
-			top: 0;
-			left: 0;
+		.scrollBox {
+			height: 300%;
+			display: flex;
+			flex-direction: column;
+			position: relative;
 
 			.videoItem {
 				width: 100%;
-				height: 100%;
+				flex: 1;
+				display: block;
 			}
 		}
 	}
